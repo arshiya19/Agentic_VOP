d2d210c74f2421984b0bc1690dcdfaf21b857125
Nikhil <nikhilsnikhil4@gmail.com>
2026-07-02 02:24:29 +0530

    fix: Updated user-data.sh to support app deploy

--- infra/app-hosting/templates/user-data.sh
+++ infra/app-hosting/templates/user-data.sh
@@ -57,9 +57,7 @@ install_docker() {
     gpg --dearmor -o /etc/apt/keyrings/docker.gpg
   chmod a+r /etc/apt/keyrings/docker.gpg
 
-  echo \
-    "deb [arch=$$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
-    https://download.docker.com/linux/ubuntu $$(lsb_release -cs) stable" | \
+  echo "deb [arch=$$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $$(lsb_release -cs) stable" | \
     tee /etc/apt/sources.list.d/docker.list > /dev/null
 
   apt-get update -y
